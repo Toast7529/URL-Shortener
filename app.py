884058322c6510bade3583b19026874bbf54ed7d
Amish Patel <64919993+Toast7529@users.noreply.github.com>
2024-10-07 20:10:13 +0100

    Compares password hash

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask, request, flash, redirect, url_for
 
 app = Flask(__name__)
 db = DB("example.db")
+
+def verifyPassword(storedPasswordHash, providedPassword):
+    return bcrypt.checkpw(providedPassword.encode('utf-8'), storedPasswordHash)
 # Login -> username, password = accessToken
 # Register -> username, password = accessToken
 # GetURLS -> accessToken = json of info
@@ -17,16 +20,17 @@ def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-    # Check if username and password match account
-    records = db.select("SELECT * FROM Account WHERE Username = ? AND Password = ?", (username, password))
-    if len(records) != 0:   # username and password are correct
-        return {'status': 200}
-    # Check if user exists, but wrong password is used
-    records = db.select("SELECT * FROM Account WHERE Username = ?", (username,))
-    if len(records) != 0:
-        return {'status': 401, 'message': 'Unauthorized: Invalid username or password'}
-    # Return 404 if account not found
-    return {'status': 404, 'message': 'User not found: The username you entered does not exist'}
+    # Fetch user password from database
+    records = db.select("SELECT Password FROM Account WHERE Username = ?", (username,))
+    if len(records) == 0:
+        return {'status': 404, 'message': 'User not found: The username you entered does not exist'}, 404
+    
+    storedPasswordHash = records[0][0]
+    if verifyPassword(storedPasswordHash, password):
+        return {'status': 200, 'message': 'Login successful'}, 200
+    else:
+        return {'status': 401, 'message': 'Unauthorized: Invalid username or password'}, 401
+
 
 
 if __name__ == '__main__':
