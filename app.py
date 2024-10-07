957ccb43dd05067855b861948f568a9b6fd5dfb2
Amish Patel <64919993+Toast7529@users.noreply.github.com>
2024-10-07 22:58:56 +0100

    Updated register and login for tokens
    Both register and login routes return JWT tokens

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import bcrypt
 import re
 from db import DB
+from tokenManager import TokenManager
 from flask import Flask, request, flash, redirect, url_for
 
 
 
 app = Flask(__name__)
 db = DB("example.db")
-
+tokenManager = TokenManager("testKey")
 def verifyPassword(storedPasswordHash, providedPassword):
     return bcrypt.checkpw(providedPassword.encode('utf-8'), storedPasswordHash)
 
@@ -27,13 +28,14 @@ def login():
     username = data.get("username")
     password = data.get("password")
     # Fetch user password from database
-    records = db.select("SELECT Password FROM Account WHERE Username = ?", (username,))
+    records = db.select("SELECT UserID, Password FROM Account WHERE Username = ?", (username,))
     if len(records) == 0:
         return {'status': 404, 'message': 'User not found: The username you entered does not exist'}, 404
     
-    storedPasswordHash = records[0][0]
+    storedPasswordHash = records[0][1]
     if verifyPassword(storedPasswordHash, password):
-        return {'status': 200, 'message': 'Login successful'}, 200
+        accessToken = tokenManager.generateAccessToken(records[0][0])
+        return {'status': 200, 'message': 'Login successful', 'token': accessToken}, 200
     else:
         return {'status': 401, 'message': 'Unauthorized: Invalid username or password'}, 401
 
@@ -42,7 +44,7 @@ def register():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-    
+
     # check password complexity
     if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_])[A-Za-z\d@$!%*?&#_]{8,}$', password) is None:
         return {'status': 400, 'message': 'Password does not meet the complexity requirements. Password must be at least 8 characters long, and contain one lowercase, uppercase, digit and special character.'}, 400
@@ -54,8 +56,9 @@ def register():
 
     # create account:
     db.insert("INSERT INTO Account (Username, Password) VALUES (?,?)", (username, hashPassword(password)))
-    print("accoint created")
-    return {'status': 201, 'message': 'Account created successfully.'}, 201
+    records = db.select("SELECT UserID FROM Account WHERE Username = ?", (username,))
+    accessToken = tokenManager.generateAccessToken(records[0][0])
+    return {'status': 201, 'message': 'Account created successfully.', 'token': accessToken}, 201
 
 if __name__ == '__main__':
     app.run(port=65000)
